fb94ff5b7863c6169e322df3c945f3a54061de3f
Ulan Temirbekov <temirbekovulanbek@gmail.com>
2021-04-16 01:20:07 +0300

    reviews added

---
+++ src/components/pages/moviedetails/details/reviews/Reviews.js
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import { fetchMovieReviews } from "../../../../../api/Api";
+import styles from "./Reviews.module.css";
+
+const Reviews = () => {
+    const [state, setState] = useState({});
+    const location = useLocation();
+
+    const getReviews = async (id) => {
+        const results = await fetchMovieReviews(id);
+        setState({ ...results });
+    };
+
+    useEffect(() => {
+        getReviews(location.state.movieId);
+
+    }, []);
+
+    const { results } = state;
+
+    return (
+
+        <div className={styles.reviews}>
+            {results !== undefined && results.length === 0 && (
+                <div className={styles.reviews_list}>
+                    <p className={styles.reviews_listContent}>
+                        We don't have any reviews for this movie
+            </p>
+                </div>
+            )}
+            <ul className={styles.reviews_list}>
+                {results &&
+                    results.map((item) => (
+                        <li className={styles.reviews_listItem} key={item.id}>
+                            <h3 className={styles.reviews_listTitle}>Author: {item.author}</h3>
+                            <p className={styles.reviews_listContent}>{item.content}</p>
+                        </li>
+                    ))}
+            </ul>
+        </div>
+    );
+};
+
+export default Reviews;
