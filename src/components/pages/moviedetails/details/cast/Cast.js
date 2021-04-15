51c2229b888eadc4830084191b9ae2b6801cf89f
Ulan Temirbekov <temirbekovulanbek@gmail.com>
2021-04-16 01:19:47 +0300

    cast added

---
+++ src/components/pages/moviedetails/details/cast/Cast.js
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import { fetchMovieCast } from "../../../../../api/Api";
+import styles from "./Cast.module.css";
+
+const Cast = () => {
+    const [state, setState] = useState({});
+    const location = useLocation();
+
+    const getCast = async (id) => {
+        const result = await fetchMovieCast(id);
+        setState({ ...result });
+    };
+
+    useEffect(() => {
+        getCast(location.state.movieId);
+
+    }, []);
+
+    const { cast } = state;
+    return (
+        <div className={styles.cast}>
+            <ul className={styles.cast_list}>
+                {cast &&
+                    cast.map((item) => (
+                        <li className={styles.cast_listItem} key={item.id}>
+                            <img
+                                className={styles.cast_listItemImg}
+                                src={`https://image.tmdb.org/t/p/w500${item.profile_path}`}
+                                alt={item.name}
+                                width="100px"
+                            />
+                            <p className={styles.cast_listItemName}> {item.name}</p>
+                            <p className={styles.cast_listItemCharacter}>
+                                Role: {item.character}
+                            </p>
+                        </li>
+                    ))}
+            </ul>
+        </div>
+    );
+};
+
+export default Cast;
