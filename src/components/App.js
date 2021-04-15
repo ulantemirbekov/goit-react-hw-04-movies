e5aff3dccf61d33f0767052c8c978a569a9f7609
Ulan Temirbekov <temirbekovulanbek@gmail.com>
2021-04-16 01:21:26 +0300

    app added

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./header/Header";
+import Main from "./main/Main";
+import "./App.css";
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Main />
+    </>
+  );
+};
+
+export default App;
