f49b3d67eeb8585cd1be0ac97a0c49a542c46e80
Ulan Temirbekov <temirbekovulanbek@gmail.com>
2021-04-16 01:21:40 +0300

    index added

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import 'modern-normalize/modern-normalize.css';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import App from "./components/App";
+import "./index.css";
 
 ReactDOM.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </BrowserRouter>,
+  document.getElementById("root")
 );
