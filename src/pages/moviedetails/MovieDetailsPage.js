6f4259ccbdf5f6e14a680f6338e890d2f5242b65
Ulan Temirbekov <temirbekovulanbek@gmail.com>
2021-04-18 13:14:34 +0300

    suspense updated

--- src/pages/moviedetails/MovieDetailsPage.js
+++ src/pages/moviedetails/MovieDetailsPage.js
@@ -55,58 +55,60 @@ const MovieDetailsPage = () => {
     } = state;
 
     return (
-        <div className={styles.details}>
-            <button className={styles.details_Btn} type="button" onClick={goBack}>
-                Go Back
-        </button>
-            <div className={styles.details_info}>
-                {poster_path && (
-                    <img
-                        src={`https://image.tmdb.org/t/p/w500${poster_path}`}
-                        alt={title}
-                        width="250px"
-                    />
-                )}
-                <div className={styles.details_infoDescription}>
-                    <h2 className={styles.details_infoTitle}>
-                        {title} ({release_date && release_date.slice(0, 4)})
-            </h2>
-                    <span className={styles.details_infoList}>
-                        User score: {vote_average * 10}%
-            </span>
-                    <h3 className={styles.details_infoTitle}>Overview</h3>
-                    <span className={styles.details_infoList}>{overview}</span>
-                    <h3 className={styles.details_infoTitle}>Genres</h3>
-                    <ul className={styles.details_infoGenre}>
-                        {genres &&
-                            genres.map((item) => (
-                                <li className={styles.details_infoGenreItem} key={item.id}>
-                                    {item.name}
-                                </li>
-                            ))}
+        <Suspense fallback={<h2>Loading...</h2>}>
+            <div className={styles.details}>
+                <button className={styles.details_Btn} type="button" onClick={goBack}>
+                    Go Back
+                </button>
+                <div className={styles.details_info}>
+                    {poster_path && (
+                        <img
+                            src={`https://image.tmdb.org/t/p/w500${poster_path}`}
+                            alt={title}
+                            width="250px"
+                        />
+                    )}
+                    <div className={styles.details_infoDescription}>
+                        <h2 className={styles.details_infoTitle}>
+                            {title} ({release_date && release_date.slice(0, 4)})
+                        </h2>
+                        <span className={styles.details_infoList}>
+                            User score: {vote_average * 10}%
+                        </span>
+                        <h3 className={styles.details_infoTitle}>Overview</h3>
+                        <span className={styles.details_infoList}>{overview}</span>
+                        <h3 className={styles.details_infoTitle}>Genres</h3>
+                        <ul className={styles.details_infoGenre}>
+                            {genres &&
+                                genres.map((item) => (
+                                    <li className={styles.details_infoGenreItem} key={item.id}>
+                                        {item.name}
+                                    </li>
+                                ))}
+                        </ul>
+                    </div>
+                </div>
+
+                <div className={styles.inform}>
+                    <h4 className={styles.details_infoTitle}>Additional information</h4>
+                    <ul className={styles.inform_list}>
+                        {moviesDetailsRoutes.map(({ name, path, exact }) => (
+                            <li className={styles.inform_listItem} key={`${id}${name}`}>
+                                <NavLink
+                                    to={{
+                                        pathname: `${match.url}${path}`,
+                                        state: { ...location.state },
+                                    }}
+                                    exact={exact}
+                                    className={styles.inform_listItemLink}
+                                    activeClassName={styles.inform_listItemLinkActive}>
+                                    {name}
+                                </NavLink>
+                            </li>
+                        ))}
                     </ul>
                 </div>
-            </div>
-            <div className={styles.inform}>
-                <h4 className={styles.details_infoTitle}>Additional information</h4>
-                <ul className={styles.inform_list}>
-                    {moviesDetailsRoutes.map(({ name, path, exact }) => (
-                        <li className={styles.inform_listItem} key={`${id}${name}`}>
-                            <NavLink
-                                to={{
-                                    pathname: `${match.url}${path}`,
-                                    state: { ...location.state },
-                                }}
-                                exact={exact}
-                                className={styles.inform_listItemLink}
-                                activeClassName={styles.inform_listItemLinkActive}>
-                                {name}
-                            </NavLink>
-                        </li>
-                    ))}
-                </ul>
-            </div>
-            <Suspense fallback={<h2>Loading...</h2>}>
+
                 <Switch>
                     {moviesDetailsRoutes.map(({ path, exact, component }) => (
                         <Route
@@ -117,8 +119,8 @@ const MovieDetailsPage = () => {
                         />
                     ))}
                 </Switch>
-            </Suspense>
-        </div >
+            </div >
+        </Suspense>
     );
 };
 
